56b0d1704178d06a2d5f402409811e3591794b93
wgs <wgshuaiit@163.com>
2021-02-15 22:31:07 +0800

    主从配置+分表

--- distributed-sharding/src/test/java/com/wanggs/sharding/DistributedShardingApplicationTest.java
+++ distributed-sharding/src/test/java/com/wanggs/sharding/DistributedShardingApplicationTest.java
@@ -2,6 +2,7 @@ package com.wanggs.sharding;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.infra.hint.HintManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,13 +30,13 @@ public class DistributedShardingApplicationTest {
         }
     }
 
-//    @Test
-//    public void findByOrderId() {
-//        //从主库查数据
-//        HintManager.getInstance().setPrimaryRouteOnly();
-//        OrderEntity byOrderId = orderDao.findByOrderId(20L);
-//        log.info("byOrderId={}", byOrderId);
-//    }
+    @Test
+    public void findByMasterOrderId() {
+        //从主库查数据
+        HintManager.getInstance().setPrimaryRouteOnly();
+        OrderEntity byOrderId = orderDao.findByOrderId(20L);
+        log.info("byOrderId={}", byOrderId);
+    }
 
     @Test
     public void findByUserId() {
